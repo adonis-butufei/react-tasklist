e5f170f165befd4b3dac414dbdebd10e09fdaeb1
ManarSayedEid <manarsayed1994@gmail.com>
2022-01-26 12:38:33 +0200

    separate mapping in tasklist component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,28 @@
+import { BsCircle } from 'react-icons/bs';
+import Button from './Button';
+import ButtonActions from '../utils/buttonActions';
+
+function TodoItem({ todo, onCompleteTodo, onDeleteTodo }) {
+
+    return (
+        <>
+            <div className={todo.isComplete ? "task done" : "task"}
+                key={todo.id}>
+
+                <div className="title"
+                    key={todo.id}
+                    onClick={() => onCompleteTodo(todo.id)}>
+
+                    <BsCircle className={todo.isComplete ? "bulleticon done" : "bulleticon"} />
+
+                    {todo.text}
+                    
+                </div>
+
+                <Button type={ButtonActions.delete} onClick={() => onDeleteTodo(todo.id)} />
+            </div>
+        </>
+    );
+}
+
+export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import TodoItems from './Todo';
+import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, onCompleteTodo, onDeleteTodo }) {
 
     return (
         <div className="tasks-container">
             <div className="task-list">
-                <TodoItems
-                    todos={todos}
-                    onCompleteTodo={onCompleteTodo}
-                    onDeleteTodo={onDeleteTodo}
-                />
+                {todos.map((todo) => (
+                    <TodoItem
+                        todo={todo}
+                        onCompleteTodo={onCompleteTodo}
+                        onDeleteTodo={onDeleteTodo}
+                    />))}
             </div>
         </div>
     );
